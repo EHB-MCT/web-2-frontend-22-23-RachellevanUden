0465105f472a32de5ae6a4d75eaacea24701d56d
Rachelle van Uden <rachelle.van.uden@student.ehb.be>
2023-08-12 18:07:34 +0200

    Changes to shoepage, dynamic api sneakers loading

--- src/main.js
+++ src/main.js
@@ -24,44 +24,45 @@ async function initUpcomingReleases() {
     let upcomingReleasesHTML = ``;
 
     sneakers.forEach((sneaker) => {
-        const htmlString = `<div class="wrapper">
-        <div onclick="location.href='/docs/shoepage.html';" class="shoe-tile" style="cursor: pointer">
-            <div id="releaseDate" class="release-date">${sneaker.releaseDate}</div> 
-            <time datetime="2014-09-20" class="icon">
+        const htmlString = `
+        <div class="wrapper">
+        <div onclick="navigateToShoePage('${sneaker._id}');" class="shoe-tile" style="cursor: pointer">
+
+            <time datetime="${sneaker.releaseDate}" class="icon">
                 <strong>${convertToMonth(new Date(sneaker.releaseDate))}</strong>
-                <span>${new Date(sneaker.releaseDate).getDay()}</span>
+                <span>${new Date(sneaker.releaseDate).getDate()}</span>
             </time>
             <img id="shoe-img" src="${sneaker.thumbnail}" alt="..." />
         </div>
         <div class="shoe-col">
             <div class="shoe-body">
-                <!--Like Button -->
+                <!-- Like Button -->
                 <button
                     class="like-button"
                     x-data="{
-    state: 'Unliked',
-    usedKeyboard: false,
-    async updateState(to) {
-    this.state = 'Saving'
-    await new Promise(resolve => setTimeout(resolve, 1000))
-    this.state = to
-    }
-    }"
-                    :class="{
-    'like unliked': state === 'Unliked',
-    'like saving': state === 'Saving',
-    'like liked': state === 'Liked',
-    'focus:outline-none': !usedKeyboard
-    }"
+                        state: 'Unliked',
+                        usedKeyboard: false,
+                        async updateState(to) {
+                        this.state = 'Saving'
+                        await new Promise(resolve => setTimeout(resolve, 1000))
+                        this.state = to
+                        }
+                        }"
+                                        :class="{
+                        'like unliked': state === 'Unliked',
+                        'like saving': state === 'Saving',
+                        'like liked': state === 'Liked',
+                        'focus:outline-none': !usedKeyboard
+                        }"
                     @click="updateState(state === 'Unliked' ? 'Liked' : 'Unliked')"
-                    @keydown.window.tab="usedKeyboard = true"
-                >
+                    @keydown.window.tab="usedKeyboard = true">
                     <span class="like-icon like-icon-state" aria-label="state" x-text="state" aria-live="polite">Unliked</span>
                 </button>
     
                 <h3 id="shoeName" class="shoe-name">${sneaker.shoeName}</h3>
+                </div>
             </div>
-        </div></div>`;
+        </div>`;
         upcomingReleasesHTML += htmlString;
     });
 
---
+++ src/shoepage.js
@@ -0,0 +1,62 @@
+'use strict';
+
+function navigateToShoePage(id) {
+    window.location.href = `/docs/shoepage.html?id=${id}`;
+}
+
+window.onload = () => {
+    // Call initProductDetails when the window loads
+    initProductDetails(sneakerId);
+};
+
+async function initProductDetails(sneakerId) {
+    // Get outer container dom
+    const outerContainer = document.querySelector('.outer-container2'); // Use class selector
+
+    // Fetch data for the specific sneaker using the id
+    const req = await fetch(`http://localhost:3000/sneaker/id?id=${sneakerId}`, { method: 'GET' });
+    const sneakers = await req.json();
+    console.log('sneaker:', sneakers); // Log the fetched sneaker data
+    const sneaker = sneakers[0]; // Access the first sneaker in the array
+    console.log(sneaker);
+
+    // Create a new div element containing the product details HTML
+    const htmlString = `
+        <h1>${sneaker.shoeName}</h1>
+        <div class="content-container">
+            <div class="container-three">
+                <time datetime="${sneaker.releaseDate}" class="icon">
+                    <strong>${convertToMonth(new Date(sneaker.releaseDate))}</strong>
+                    <span>${new Date(sneaker.releaseDate).getDate()}</span>
+                </time>
+                <img id="shoe-img-detail" src="${sneaker.thumbnail}" alt="...">
+            </div>
+            <div class="container-four">
+                <ul class="detail-list">
+                    <li>
+                        <h3>Productdetails</h3>
+                    </li>
+                    <li>
+                        <h4>${sneaker.shoeName}</h4>
+                    </li>
+                    <li>
+                        <h4>${sneaker.brand}</h4>
+                    </li>
+                    <li>
+                        <h4>${sneaker.description}</h4>
+                    </li>
+                    <li>
+                        <h4>${sneaker.releaseDate}</h4>
+                    </li>
+                    <li>
+                        <h4>${sneaker.retailPrice}</h4>
+                    </li>
+                </ul>
+            </div>
+        </div>`;
+
+   // Insert the product details HTML into the outer container
+    outerContainer.innerHTML = htmlString;
+}
+
+initProductDetails();
